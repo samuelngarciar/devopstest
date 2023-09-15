336f26d02a33543e8f36eaaeada532254dbf091f
samuelngarciar <84947793+samuelngarciar@users.noreply.github.com>
2023-09-15 08:12:37 -0400

    Update app.py

--- src/python/app.py
+++ src/python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'microservice devopstest -4'
+    return 'microservice devopstest go'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
